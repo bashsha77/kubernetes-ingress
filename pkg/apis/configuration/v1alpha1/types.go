fa537f4590357edf64b52d5204bce430292e5006
Sean O'Neill <s.oneill@f5.com>
2021-02-19 13:23:08 +0000

    Add status field to TransportServer resource

--- pkg/apis/configuration/v1alpha1/types.go
+++ pkg/apis/configuration/v1alpha1/types.go
@@ -50,13 +50,20 @@ type GlobalConfigurationList struct {
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 // +kubebuilder:validation:Optional
 // +kubebuilder:resource:shortName=ts
+// +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="State",type=string,JSONPath=`.status.state`,description="Current state of the VirtualServer. If the resource has a valid status, it means it has been validated and accepted by the Ingress Controller."
+// +kubebuilder:printcolumn:name="Reason",type=string,JSONPath=`.status.reason`
+// +kubebuilder:printcolumn:name="IP",type=string,JSONPath=`.status.externalEndpoints[*].ip`
+// +kubebuilder:printcolumn:name="Ports",type=string,JSONPath=`.status.externalEndpoints[*].ports`
+// +kubebuilder:printcolumn:name="Age",type=date,JSONPath=`.metadata.creationTimestamp`
 
 // TransportServer defines the TransportServer resource.
 type TransportServer struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec TransportServerSpec `json:"spec"`
+	Spec   TransportServerSpec   `json:"spec"`
+	Status TransportServerStatus `json:"status"`
 }
 
 // TransportServerSpec is the spec of the TransportServer resource.
@@ -115,6 +122,20 @@ type SessionParameters struct {
 // Action defines an action.
 type Action struct {
 	Pass string `json:"pass"`
+}
+
+// TransportServerStatus defines the status for the TransportServer resource.
+type TransportServerStatus struct {
+	State             string             `json:"state"`
+	Reason            string             `json:"reason"`
+	Message           string             `json:"message"`
+	ExternalEndpoints []ExternalEndpoint `json:"externalEndpoints,omitempty"`
+}
+
+// ExternalEndpoint defines the IP and ports used to connect to this resource.
+type ExternalEndpoint struct {
+	IP    string `json:"ip"`
+	Ports string `json:"ports"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
